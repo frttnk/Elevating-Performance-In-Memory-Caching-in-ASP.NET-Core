8c8266e9bfe18f80023b87c2ef97a4a7d9054c8b
frttnk <firattonak@gmail.com>
2022-12-13 01:21:35 -0500

    implemented in-memory cache

---
+++ InMemoryCaching/InMemoryCaching/Controllers/InMemoryTestController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InMemoryTestController : ControllerBase
+    {
+        private readonly IMemoryCache _cache;
+
+        public InMemoryTestController(IMemoryCache cache)
+        {
+            this._cache = cache;
+        }
+
+        [HttpGet]
+        public IActionResult InMemoryCache()
+        {
+            string result =  "";
+            var cacheResult = _cache.Get<string>("CacheKey") ?? "";
+            if (cacheResult=="")
+            {
+                result = FakeDbConnection();
+                // you can put everything as a cachekey.
+                // you can configure cache time up to your requirements.
+                _cache.Set<string>("CacheKey", result, TimeSpan.FromMinutes(2));
+            }
+            return Ok(result);
+        }
+
+
+        private string FakeDbConnection()
+        {
+            return "Connected Db";
+        }
+        
+    }
+}
